eb5cee9dc9ac02ef55d7a7e02a0a653619902bc7
Samuel Fendell <sfendell91@gmail.com>
2013-02-11 22:29:44 -0800

    this version works on local

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-DATABASES = {
-             'default' : dj_database_url.config()}#default=os.environ["DATABASE_URL"])}
+#import dj_database_url
+#DATABASES = {
+#             'default' : dj_database_url.config()}#default=os.environ["DATABASE_URL"])}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
